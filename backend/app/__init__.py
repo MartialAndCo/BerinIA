8417e8bd7296b783cab87f23fcbae2b60878ae48
yann <yannrosemak@gmail.com>
2025-05-08 19:58:35 +0000

    Sync current VPS state

--- backend/app/__init__.py
+++
@@ -1,5 +0,0 @@
-from app.database.session import engine
-from app.database.base_class import Base
-
-# Créer les tables
-Base.metadata.create_all(bind=engine)
--- backend/app/crud/__init__.py
+++
@@ -1,5 +0,0 @@
-from .agent import *
-from .campaign import *
-from .lead import *
-from .niche import *
-from .agent_log import *
